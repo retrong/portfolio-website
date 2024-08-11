52db8dafde641405e104d8b9a684e64fa530709e
retrong <eguobabarth@gmail.com>
2024-08-11 19:41:00 +0100

    modified component styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ import { Button } from './ui/button';
 
 const Header = () => {
   return (
-		<header className="py-4 xl:py-2 my-8 px-4 text-white w-full">
+		<header className="py-4 xl:py-2 xl:my-8 my-0 px-4 text-white w-full">
 			<div className="container mx-auto lg:w-[60%] w-full flex justify-between gap-12 py-2 lg:pl-12 pl-4 items-center rounded-full lg:bg-accent/30 bg-none ">
 				{/* logo */}
 				<Link href="/" className="relative">
