7bababdb6288f7eff42b827b632fefa321a33488
retrong <eguobabarth@gmail.com>
2024-10-07 22:29:32 +0100

    made changes to information on about me info

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,6 @@ export default function Home() {
                                 <FiDownload className='text-xl' />
                             </Button>
                         </a>
-                        <a href="EguobaBarth.pdf" download="eguoba Bartholomew cv">Download</a>
                         <div className='mb-8 xl:mb-0'>
                             <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
                         </div>
