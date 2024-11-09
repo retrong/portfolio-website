0d660175218775f810f7a472ee5dc09e93b9a241
retrong <eguobabarth@gmail.com>
2024-11-09 21:05:25 +0100

    removed transition and updated loader

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
 		<div className='flex justify-center items-center'>
-			<div className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-accent border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
+			<div className="inline-block h-20 w-20 animate-spin rounded-full border-2 border-solid border-accent border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
 		</div>
 	);
 }
